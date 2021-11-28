280fad8efa8b142b2bf7f80f336ad6ac06f5d338
Andrew Hudson <ahudson@gmail.com>
2021-11-28 20:23:30 +0000

    bump astro to 0.21.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,12 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: [],
 
-  devOptions: {
-    port: 8888,
-    // /**
-    //  * Configure The trailing slash behavior of URL route matching:
-    //  *   'always' - Only match URLs that include a trailing slash (ex: "/foo/")
-    //  *   'never' - Never match URLs that include a trailing slash (ex: "/foo")
-    //  *   'ignore' - Match URLs regardless of whether a trailing "/" exists
-    //  * Default: 'always'
-    //  */
-    // trailingSlash?: 'always' | 'never' | 'ignore';
+/** @type {import('astro').AstroUserConfig} */
+export default {
+  renderers: [],
+  vite: {
+    plugins: [],
   },
-});
+  devOptions: {
+    port: 8888
+  }
+};
