5b8ebbec26f444c75411d81c88668fbe96dead9a
Andrew Hudson <ahudson@scalable.com>
2023-11-20 15:27:20 +0000

    add weeknote api

---
+++ src/pages/api/date/week-number.ts
@@ -0,0 +1,15 @@
+// Outputs: /builtwith.json
+export async function GET() {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate.getFullYear(), 0, 1);
+  const dateDifference: number =
+    (currentDate as unknown as number) - (startDate as unknown as number);
+  const days = Math.floor(dateDifference / (24 * 60 * 60 * 1000));
+
+  const weekNumber = Math.ceil(days / 7);
+  return new Response(
+    JSON.stringify({
+      weekNumber,
+    })
+  );
+}
