3f1b6e1d8eb7de89c0fe72293637b9aa99e04c66
Andrew Hudson <ahudson@gmail.com>
2024-11-28 18:09:28 +0100

    one commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,25 @@
 /** @type {import('astro').AstroUserConfig} */
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import svelte from '@astrojs/svelte'
-import robotsTxt from 'astro-robots-txt'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
 
-import { robotsTxtConfig } from './robots'
-
-import icon from 'astro-icon'
+import { robotsTxtConfig } from "./robots";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), robotsTxt(robotsTxtConfig), mdx(), react(), icon()],
-	scopedStyleStrategy: 'class',
-	vite: {
-		plugins: [],
-		optimizeDeps: {
-			exclude: ['@resvg/resvg-js'],
-		},
-	},
-	server: {
-		port: 8888,
-	},
-	site: 'https://andrewhudson.dev',
-})
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+    routing: "manual",
+  },
+  integrations: [lit(), svelte(), robotsTxt(robotsTxtConfig), mdx()],
+  scopedStyleStrategy: "class",
+  vite: {
+    plugins: [],
+  },
+  server: {
+    port: 8888,
+  },
+  site: "https://andrewhudson.dev",
+});
---
+++ src/content/blog/translation-test/index.md
@@ -0,0 +1,9 @@
+---
+title: "Translation Test"
+date: 2024-04-19
+draft: false
+language: "en"
+hasTranslation: true
+---
+
+This is an english version of the post
