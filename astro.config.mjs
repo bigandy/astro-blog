2f983559645d7d1a5535f7801e82cdb32cd428aa
Andrew Hudson <ahudson@gmail.com>
2024-12-12 11:04:26 +0100

    use astro:env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,41 @@
 /** @type {import('astro').AstroUserConfig} */
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import svelte from '@astrojs/svelte'
-import robotsTxt from 'astro-robots-txt'
-import mdx from '@astrojs/mdx'
+import { defineConfig, envField } from "astro/config";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
 
-import { robotsTxtConfig } from './robots'
+import { robotsTxtConfig } from "./robots";
 
-import icon from 'astro-icon'
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), robotsTxt(robotsTxtConfig), mdx(), react(), icon()],
-	scopedStyleStrategy: 'class',
-	vite: {
-		plugins: [],
-		optimizeDeps: {
-			exclude: ['@resvg/resvg-js'],
-		},
-	},
-	server: {
-		port: 8888,
-	},
-	site: 'https://andrewhudson.dev',
-})
+  env: {
+    schema: {
+      NOTION_DB: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      NOTION_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+        startsWith: "secret_",
+      }),
+    },
+  },
+  integrations: [svelte(), robotsTxt(robotsTxtConfig), mdx(), react(), icon()],
+  scopedStyleStrategy: "class",
+  vite: {
+    plugins: [],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  server: {
+    port: 8888,
+  },
+  site: "https://andrewhudson.dev",
+});
