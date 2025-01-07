190b0914fbf5abdeedcf1ad5a685723aecf0d41f
Andrew Hudson <ahudson@gmail.com>
2025-01-07 13:14:35 +0100

    continue on cv: create more components, update copy, update design and break-point

--- src/pages/cv/data.ts
+++ src/pages/cv/data.ts
@@ -1,3 +1,12 @@
+export const personalInformation = {
+    name: "Andrew JD Hudson",
+    title: "Senior Front-end Engineer",
+    email: "ahudson@gmail.com",
+    website: "https://andrewhudson.dev",
+    phone: "+33608985800",
+    location: "Grenoble, France",
+};
+
 export interface Role {
     [key: string]: {
         url: string;
@@ -20,9 +29,9 @@ export const roles: Role = {
         showFullDates: true,
         summary: [
             "Web app development with React.js, Typescript, and D3 taking designs in Figma and Confluence and turning them into stories, tasks and sub-tasks and then delivering the work.",
-            "I mentored junior developers and reviewed merge requests across the front-end team ensuring a consistently high quality in our product.",
-            "Estimating, planned and documented new features with product and design.",
-            "Creating new features and speccing out API changes with the backend team",
+            "Mentored junior developers and reviewed merge requests across the front-end team ensuring a consistently high quality product.",
+            "Estimated, planned and documented new features with product and design.",
+            "Created new features and specced out API changes with the backend team",
             // "<li>Creating API contract with the backend API developer</li>",
             // <li>Mocking data with express to simulate the API response until backend finished API work</li>",
         ],
@@ -36,7 +45,7 @@ export const roles: Role = {
         showFullDates: true,
         summary: [
             "Web app development with CSS-in-JS, Next.js, GraphQL and Typescript.",
-            "I created two Progressive Web Apps and concentrated on improving the performance of our websites and products. This helped our improve our SEO and our Lighthouse Scores and Web Page Test performance.",
+            "I created two Progressive Web Apps and concentrated on improving the performance of our websites and products. This helped improve our SEO, Lighthouse Scores, and Web Page Test performance.",
             "I created a component library using Storybook to showcase and document.",
             "I mentored the rest of the engineering team on front-end topics regularly producing demos and presentations on what I made and learned.",
             // "I created a component library using Storybook to showcase and document.",
@@ -62,7 +71,7 @@ export const roles: Role = {
         start: "March 2016",
         end: "August 2016",
         summary: [
-            "I creating new components and pages using React/Relay and Less from Photoshop designs.",
+            "I created new components and pages using React/Relay and Less from Photoshop designs.",
             "I integrated Webpack into the development process to allow for code modularity, linting, and writing modular ES6 JS.",
             "I implemented the application of BEM code methodology for new CSS components.",
         ],
@@ -74,7 +83,7 @@ export const roles: Role = {
         start: "2013",
         end: "2016",
         summary: [
-            "I created highly customised WordPress sites from Photoshop designs, with custom post-types, taxonomies, meta boxes, shortcodes and functions.",
+            "I created highly customised WordPress sites from Photoshop designs, with custom post types, taxonomies, meta boxes, shortcodes and functions.",
             "The sites were performant, using best practices and technologies, built within budget and in timeframes.",
             "Pre-launch client training, dealing with any client feedback via Basecamp or telephone.",
         ],
@@ -88,7 +97,110 @@ export const roles: Role = {
         summary: [
             "I built highly accessible websites for clients including HSBC, RBS and Grant Thornton.",
             "Using CMSs such as WordPress, CQ5 and Sitecore to allow the client to control all content on their site.",
-            "Knowledge Sharing through weekly masterclass sessions (I ran three : WordPress; CSS3; and Sass).",
+            "Knowledge Sharing through weekly masterclass sessions (I ran three: WordPress; CSS3; and Sass).",
         ],
     },
 };
+
+export const skills = [
+    "React",
+    "Typescript",
+    "CSS",
+    "Next.js",
+    "Astro",
+    "Svelte",
+    "Lit",
+    "Svelte",
+    "Chrome Extension Development",
+    "Electron",
+    "GraphQL",
+    "Sass",
+    "CSS-in-JS",
+    "Website Performance Optimisation",
+    "Databases (Firebase, Postgres, MySQL)",
+    "Serverless functions (Firebase, AWS)",
+    "Storybook",
+    "Testing with Cypress and Jest",
+    "Accessibility",
+    "SEO",
+    "Git",
+];
+
+export interface SocialLink {
+    href: string;
+    name: string;
+    shortLink: string;
+}
+export const socialLinks: Array<SocialLink> = [
+    {
+        href: "https://github.com/bigandy",
+        name: "Github",
+        shortLink: "github.com/bigandy",
+    },
+    {
+        href: "https://codepen.io/bigandy",
+        name: "Codepen",
+        shortLink: "codepen.io/bigandy",
+    },
+    {
+        href: "https://linkedin.com/in/andrew-hudson-software-engineer/",
+        name: "Linkedin",
+        shortLink: "linkedin.com/in/andrew-hudson-software-engineer/",
+    },
+    {
+        href: "https://andrewhudson.dev",
+        name: "Personal Site",
+        shortLink: "andrewhudson.dev",
+    },
+];
+
+export const portfolioLinks = [
+    {
+        href: "https://www.scalable.com/",
+        name: "Scalable",
+        shortLink: "scalable.com",
+        description:
+            "a digital experience web app for visualising Productivity and Digital Workplace Efficiency using React, Typescript, and D3.",
+    },
+    {
+        href: "https://truenorth.io",
+        name: "TrueNorth",
+        shortLink: "truenorth.io",
+        description:
+            "a marketing web app using NextJS, Typescript, GraphQL, Apollo, Prisma, with a Material-UI based component library.",
+    },
+    {
+        href: "https://sereneapp.com",
+        name: "Serene App",
+        shortLink: "sereneapp.com",
+        description:
+            "Productivity Application for MacOS users using Electron, Firebase, and React. With companion Chrome, Firefox and Edge extensions.",
+    },
+    {
+        href: "https://photocrowd.com",
+        name: "photocrowd.com",
+        shortLink: "photocrowd.com",
+        description:
+            "front-end development with React/Relay, Less, and Webpack",
+    },
+    {
+        href: "https://allistergodfrey.com/",
+        name: "allistergodfrey.com",
+        shortLink: "allistergodfrey.com",
+        description:
+            "a custom responsive WordPress site with many interactive  features, galleries, and css transitions",
+    },
+    {
+        href: "https://ojwmanagement.com/",
+        name: "ojwmanagement.com",
+        shortLink: "ojwmanagement.com",
+        description:
+            "a single-page responsive WordPress site with flexbox, responsive images, and SVG",
+    },
+    {
+        href: "https://blog.oup.com/",
+        name: "blog.oup.com",
+        shortLink: "blog.oup.com",
+        description: "responsive WordPress blog for Oxford University Press",
+    },
+];
