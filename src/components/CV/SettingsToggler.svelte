ea44b133b0ad7e8013e69afece7051d861cd85e7
Andrew Hudson <ahudson@gmail.com>
2025-01-08 12:27:39 +0100

    cv: continue improving theme selector

--- src/components/CV/SettingsToggler.svelte
+++ src/components/CV/SettingsToggler.svelte
@@ -58,8 +58,9 @@
     </button>
 
     <div class="box" popover="auto" id="settings-menu">
-        <h2>This will have the settings in when the user clicks the image</h2>
+        <h2>Theme Picker</h2>
 
+        <label for="color">Theme color:</label>
         <input
             type="color"
             name="color"
@@ -67,8 +68,8 @@
             oninput={handleColor}
             value={themeColor}
         />
-        <button onclick={resetTheme}>Reset Theme</button>
-        <button onclick={closePopover}>Close Popover</button>
+        <button onclick={resetTheme} class="reset">Reset Theme</button>
+        <button onclick={closePopover} class="close">x</button>
     </div>
 </div>
 
@@ -99,20 +100,47 @@
         filter: drop-shadow(0px 0px 6px var(--theme-accent));
     }
 
+    label {
+        font-size: 14px;
+    }
+
     .box {
         /* display: block; */
+        width: calc(anchor-size(width) * 3);
         position: absolute;
         inset: auto;
         margin: 0;
         position-anchor: --image;
         top: calc(anchor(bottom) + 10px);
         right: anchor(right);
-
         padding: 1rem;
         border-radius: 0.25rem;
 
         h2 {
-            all: unset;
+            font-weight: bold;
+            display: block;
+            margin-top: 0;
+            margin-bottom: 0.5rem;
+            padding-top: 0;
+        }
+
+        input {
+            width: 100%;
+            margin-bottom: 1rem;
+        }
+
+        .reset {
+            width: 100%;
+        }
+
+        .close {
+            position: absolute;
+            top: 0.5rem;
+            right: 0.5rem;
+            background: transparent;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
         }
     }
 
