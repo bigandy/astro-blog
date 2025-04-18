b723dc55837d4964d12cc7190fa8d7832043dc79
Andrew Hudson <ahudson@gmail.com>
2025-04-18 15:55:33 +0200

    refactor, use source code of script.mjs in post

--- script.mjs
+++ script.mjs
@@ -12,20 +12,10 @@ const newFileLocation = `src/content/blog/${postTitleHyphenated}.md`;
 await $`cp template-content/template-blog-post.md ${newFileLocation}`;
 const PUB_DATE = await $`date +%Y-%m-%d`;
 
-const f1 = tmpfile();
-await $`awk -v postTitle=${postTitle} '{sub(/TITLE/,postTitle)}1' ${newFileLocation} | awk -v pubDate=${PUB_DATE} '{sub(/PUBDATE/,pubDate)}1' > ${f1} && mv ${f1} ${newFileLocation}`;
+const tempFile = tmpfile();
+await $`awk -v postTitle=${postTitle} '{sub(/TITLE/,postTitle)}1' ${newFileLocation} | awk -v pubDate=${PUB_DATE} '{sub(/PUBDATE/,pubDate)}1' > ${tempFile} && mv ${tempFile} ${newFileLocation}`;
 
 await $(`open ${`http://localhost:1234/blog/${postTitleHyphenated}`}`);
 
 await $`code ${newFileLocation}:8:7 -g`;
 await $`npm run dev -- --port 1234`;
-
-// const startServer =
-//     (await question("Do you want to start the server? (Y) / N")).trim() || "Y";
-
-// if (startServer === "Y") {
-//     console.log("Start server please");
-//     await $`npm run dev -- --open`;
-// } else {
-//     console.log("no server running");
-// }
