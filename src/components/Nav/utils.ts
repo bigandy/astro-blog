7376a78375099cfc35fcb29ead8b00c5896653db
Andrew Hudson <ahudson@gmail.com>
2025-06-16 13:38:56 +0200

    dont need these

--- src/components/Nav/utils.ts
+++ src/components/Nav/utils.ts
@@ -1,8 +1,4 @@
-export const getNavClassName = (
-    url: string,
-    requestPathname: string,
-    // locale: string,
-) => {
+export const getNavClassName = (url: string, requestPathname: string) => {
     // return early external links
     if (url.includes("http")) {
         return "";
@@ -13,8 +9,6 @@ export const getNavClassName = (
     // if (locale === "fr") {
     //     requestPathnameWithNoSlashes.replace("fr", "");
     // }
-
-    console.log({ requestPathname, url });
 
     const className =
         requestPathname.replace("/fr", "") === url ? "active" : "";
