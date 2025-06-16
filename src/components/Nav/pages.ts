12501aecd642fd7de684608b11469a69d2e14719
Andrew Hudson <ahudson@gmail.com>
2025-06-16 12:39:21 +0200

    add more Fr versions of pages

--- src/components/Nav/pages.ts
+++ src/components/Nav/pages.ts
@@ -1,147 +1,148 @@
 const pages: {
-//     [key: string]: {
-//         url: string;
-//         title: { default: string; fr?: string };
-//         location: "header" | "footer" | "both";
-//         external?: boolean;
-//     };
-// } = {
-//     "sites-i-like": {
-//         url: "/sites-i-like",
-//         title: "Sites I like",
-//         location: "footer",
-//     },
+    //     [key: string]: {
+    //         url: string;
+    //         title: { default: string; fr?: string };
+    //         location: "header" | "footer" | "both";
+    //         external?: boolean;
+    //     };
+    // } = {
+    //     "sites-i-like": {
+    //         url: "/sites-i-like",
+    //         title: "Sites I like",
+    //         location: "footer",
+    //     },
 
-//     about: {
-//         url: "/about",
-//         title: "About",
-//         location: "header",
-//     },
+    //     about: {
+    //         url: "/about",
+    //         title: "About",
+    //         location: "header",
+    //     },
 
-//     tools: {
-//         url: "/tools",
-//         title: "Tools",
-//         location: "footer",
-//     },
+    //     tools: {
+    //         url: "/tools",
+    //         title: "Tools",
+    //         location: "footer",
+    //     },
 
-//     archive: {
-//         url: "/blog",
-//         title: "Blog",
-//         location: "header",
-//     },
-//     cv: {
-//         url: "/cv",
-//         title: "CV",
-//         location: "header",
-//     },
+    //     archive: {
+    //         url: "/blog",
+    //         title: "Blog",
+    //         location: "header",
+    //     },
+    //     cv: {
+    //         url: "/cv",
+    //         title: "CV",
+    //         location: "header",
+    //     },
 
-//     experiments: {
-//         url: "/experiments",
-//         title: "Experiments",
-//         location: "footer",
-//     },
+    //     experiments: {
+    //         url: "/experiments",
+    //         title: "Experiments",
+    //         location: "footer",
+    //     },
 
-//     weeknotes: {
-//         url: "/weeknotes",
-//         title: "Weeknotes",
-//         location: "footer",
-//     },
+    //     weeknotes: {
+    //         url: "/weeknotes",
+    //         title: "Weeknotes",
+    //         location: "footer",
+    //     },
 
-//     bookshelf: {
-//         url: "/bookshelf",
-//         title: "Bookshelf",
-//         location: "footer",
-//     },
+    //     bookshelf: {
+    //         url: "/bookshelf",
+    //         title: "Bookshelf",
+    //         location: "footer",
+    //     },
 
-//     now: {
-//         url: "/now",
-//         title: "Now",
-//         location: "footer",
-//     },
-// =======
-  [key: string]: {
-    url: string;
-    title: { default: string; fr?: string };
-    location: "header" | "footer" | "both";
-    external?: boolean;
-  };
+    //     now: {
+    //         url: "/now",
+    //         title: "Now",
+    //         location: "footer",
+    //     },
+    // =======
+    [key: string]: {
+        url: string;
+        title: { default: string; fr?: string };
+        location: "header" | "footer" | "both";
+        external?: boolean;
+    };
 } = {
-  "sites-i-like": {
-    url: "/sites-i-like",
-    title: {
-      default: "Sites I like",
-    },
-    location: "footer",
-  },
+    // "sites-i-like": {
+    //     url: "/sites-i-like",
+    //     title: {
+    //         default: "Sites I like",
+    //         fr: "Sites que j'aime",
+    //     },
+    //     location: "footer",
+    // },
 
-  about: {
-    url: "/about",
-    title: {
-      default: "About",
-      fr: "À propos",
+    about: {
+        url: "/about",
+        title: {
+            default: "About",
+            fr: "À propos",
+        },
+        location: "header",
     },
-    location: "header",
-  },
 
-  demos: {
-    url: "/experiments",
-    location: "footer",
-    title: {
-      default: "Experiments",
-      fr: "Expériences",
+    demos: {
+        url: "/experiments",
+        location: "footer",
+        title: {
+            default: "Experiments",
+            fr: "Expériences",
+        },
     },
-  },
 
-  tools: {
-    url: "/tools",
-    title: {
-      default: "Tools",
-    },
-    location: "footer",
-  },
+    // tools: {
+    //     url: "/tools",
+    //     title: {
+    //         default: "Tools",
+    //     },
+    //     location: "footer",
+    // },
 
-  blog: {
-    url: "/",
-    title: {
-      default: "Archive",
-      fr: "Les Archives",
+    blog: {
+        url: "/",
+        title: {
+            default: "Archive",
+            fr: "Les Archives",
+        },
+        location: "header",
     },
-    location: "header",
-  },
 
-  weeknotes: {
-    url: "/weeknotes",
-    location: "header",
-    title: {
-      default: "Weeknotes",
+    weeknotes: {
+        url: "/weeknotes",
+        location: "header",
+        title: {
+            default: "Weeknotes",
+        },
     },
-  },
 
-  now: {
-    url: "/now",
-    title: {
-      default: "Now",
+    now: {
+        url: "/now",
+        title: {
+            default: "Now",
+        },
+        location: "footer",
     },
-    location: "footer",
-  },
 
-  bookshelf: {
-    url: "/bookshelf",
-    title: {
-      default: "Bookshelf",
-      fr: "Bibliothèque",
+    bookshelf: {
+        url: "/bookshelf",
+        title: {
+            default: "Bookshelf",
+            fr: "Bibliothèque",
+        },
+        location: "header",
     },
-    location: "header",
-  },
 
-  cv: {
-    url: "/cv",
-    location: "header",
-    title: {
-      default: "CV",
-      fr: "CV",
+    cv: {
+        url: "/cv",
+        location: "header",
+        title: {
+            default: "CV",
+            fr: "CV",
+        },
     },
-  },
 };
 
 export const headerPages = Object.values(pages).filter(({ location }) =>
