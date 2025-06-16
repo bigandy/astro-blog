0e3101bb378874016b1a95f5f17681f73fbb8a48
Andrew Hudson <ahudson@gmail.com>
2025-06-16 14:54:17 +0200

    weeknotes progress

--- src/components/Nav/getNavClassName.ts
+++ src/components/Nav/getNavClassName.ts
@@ -8,5 +8,9 @@ export const getNavClassName = (url: string, requestPathname: string) => {
         return "active";
     }
 
+    if (url === "/weeknotes" && requestPathname.includes("/weeknotes/")) {
+        return "active";
+    }
+
     return requestPathname.replace("/fr", "") === url ? "active" : "";
 };
