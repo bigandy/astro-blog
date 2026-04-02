b4401d414844d02b03b12b959294de2bbdcb5281
Andrew Hudson <ahudson@gmail.com>
2026-04-02 20:23:48 +0200

    ShadowMaker component: make it work as I intended originally and now had the time

--- src/components/CV/ShadowMaker.svelte
+++ src/components/CV/ShadowMaker.svelte
@@ -1,60 +1,58 @@
 <script lang="ts">
-import { onMount } from "svelte";
+    import { onMount } from "svelte";
+    let container: HTMLElement;
 
-const container = document.querySelector(".container") as HTMLElement;
+    const { children, spread = 5, color = "" } = $props();
 
-const handleMouseMove = (evt) => {
-	if (!container) {
-		return;
-	}
-	const { height: containerHeight, width: containerWidth } =
-		container.getBoundingClientRect();
+    const trackTheMouse = (evt: MouseEvent) => {
+        // get the coords of the container.
+        const { top, left, width, height } = container.getBoundingClientRect();
 
-	// let x = evt.clientX / containerWidth;
-	// let y = evt.clientY / containerHeight;
+        // Center of the page
+        const center = {
+            top: top + height / 2,
+            left: left + width / 2,
+        };
 
-	// Center of the container
-	const center = {
-		top: containerHeight / 2,
-		left: containerWidth / 2,
-	};
+        // distance from the center to the mouse cursor
+        const actual = {
+            left: evt.clientX - center.left,
+            top: evt.clientY - center.top,
+        };
 
-	// distance from the center to the mouse cursor
-	const actual = {
-		left: evt.clientX - center.left,
-		top: evt.clientY - center.top,
-	};
+        container.style.setProperty("--top", actual.top.toString());
+        container.style.setProperty("--left", actual.left.toString());
+    };
 
-	// console.log({ evt, center, actual });
+    onMount(() => {
+        document.addEventListener("mousemove", trackTheMouse);
 
-	// get the angle from the opposite/adjacent. Trigonometry!
-	// let angle = getAngleFromOppositeAdjacent(actual.top, actual.left);
-
-	// angle += actual.left >= 0 ? 90 : 270;
-
-	container.style.setProperty("--top", (actual.top *= -1).toString());
-	container.style.setProperty("--left", (actual.left *= -1).toString());
-};
-
-const { children } = $props();
-
-const getAngleFromOppositeAdjacent = (top, left) => {
-	return (Math.atan(top / left) * 180) / Math.PI;
-};
+        return () => document.removeEventListener("mousemove", trackTheMouse);
+    });
 </script>
 
-<div role="application" class="container" onmousemove={handleMouseMove}>
+<!-- bind:this is like ref={myRef} in react -->
+<div
+    role="application"
+    id="container"
+    bind:this={container}
+    style={`--multiplier: ${spread}; --color: ${color !== "" ? color : "unset"}`}
+>
     {@render children?.()}
 </div>
 
 <style>
     div {
-        margin-inline: auto;
+        /* margin-inline: auto; */
         display: inline-block;
+        width: max-content;
         border: 1px solid;
+        --x: calc(var(--left, 1) * -0.1px * var(--multiplier, 1));
+        --y: calc(var(--top, 1) * -0.1px * var(--multiplier, 1));
+        --spread: 5px;
+
         filter: drop-shadow(
-            calc(var(--left, 1) * 0.1px) calc(var(--top, 1) * 0.1px) 5px
-                var(--theme-accent)
+            var(--x) var(--y) var(--spread) var(--color, var(--theme-accent))
         );
 
         :global(img) {
