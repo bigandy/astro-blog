4c71048f103a98946ce367ad21d0eca7b70437eb
Andrew Hudson <ahudson@gmail.com>
2026-04-10 16:24:34 +0200

    add content to rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,17 @@
-import type { APIRoute } from "astro";
 import rss from '@astrojs/rss';
 import { getRSSPosts } from '@components/Blog/utils/getAllPosts';
+import type { APIRoute } from "astro";
+
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export const GET = (async (context) => {
   const posts = await getRSSPosts();
 
   return rss({
-    // `<title>` field in output xml
     title: "Andrew JD Hudson's Blog",
-    // `<description>` field in output xml
     description: "Recent content in Articles & Experiments by Andrew JD Hudson",
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#site
@@ -20,6 +23,9 @@ export const GET = (async (context) => {
       pubDate: post.data.date as unknown as Date,
       description: post.data.description,
       link: `/blog/${post.id}/`,
+      content: post.body && sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
     // (optional) inject custom xml
     customData: `<language>en-gb</language><generator>Astro</generator>`,
