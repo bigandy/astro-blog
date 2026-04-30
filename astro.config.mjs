a04a78f4255d5d54959b8597a32f06b59ca7747c
Andrew Hudson <ahudson@gmail.com>
2026-04-30 14:10:38 +0200

    start on selectedcontent article

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,55 +4,51 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import { defineConfig, envField } from "astro/config";
-import lit from '@semantic-ui/astro-lit';
+import lit from "@semantic-ui/astro-lit";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: 'always',
-	env: {
-		schema: {
-			NOTION_DB: envField.string({
-				context: "server",
-				access: "secret",
-				optional: false,
-			}),
-			NOTION_KEY: envField.string({
-				context: "server",
-				access: "secret",
-				optional: false,
-				startsWith: "secret_",
-			}),
-		},
-	},
-	i18n: {
-		locales: ["en", "fr"],
-		defaultLocale: "en",
-	},
-	integrations: [
-		svelte(),
-		mdx(),
-		react(),
-		lit(),
-	],
-	markdown: {
-		syntaxHighlight: "prism",
-	},
-	scopedStyleStrategy: "class",
-	server: {
-		port: 8888,
-	},
-	site: "https://andrewhudson.dev",
-	devToolbar: {
-		enabled: false,
-	},
-	build: {
-		inlineStylesheets: "always",
-	},
-	experimental: {
-		rustCompiler: true,
-		queuedRendering: {
-			enabled: true,
-			contentCache: true,
-		},
-	},
+    trailingSlash: "always",
+    env: {
+        schema: {
+            NOTION_DB: envField.string({
+                context: "server",
+                access: "secret",
+                optional: false,
+            }),
+            NOTION_KEY: envField.string({
+                context: "server",
+                access: "secret",
+                optional: false,
+                startsWith: "secret_",
+            }),
+        },
+    },
+    i18n: {
+        locales: ["en", "fr"],
+        defaultLocale: "en",
+    },
+    integrations: [expressiveCode(), svelte(), mdx(), react(), lit()],
+    // markdown: {
+    // 	syntaxHighlight: "prism",
+    // },
+    scopedStyleStrategy: "class",
+    server: {
+        port: 8888,
+    },
+    site: "https://andrewhudson.dev",
+    devToolbar: {
+        enabled: false,
+    },
+    build: {
+        inlineStylesheets: "always",
+    },
+    experimental: {
+        rustCompiler: true,
+        queuedRendering: {
+            enabled: true,
+            contentCache: true,
+        },
+    },
 });
