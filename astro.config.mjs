b15f31d17a73d330d5755ff506e32aa6a5a449bc
Andrew Hudson <ahudson@gmail.com>
2026-06-04 08:43:39 +0200

    try out markdown-satteri

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,46 +6,52 @@ import svelte from "@astrojs/svelte";
 import { defineConfig, envField } from "astro/config";
 import lit from "@semantic-ui/astro-lit";
 import expressiveCode from "astro-expressive-code";
+import { satteri } from "@astrojs/markdown-satteri";
 
 // https://astro.build/config
 export default defineConfig({
-    trailingSlash: "always",
-    env: {
-        schema: {
-            NOTION_DB: envField.string({
-                context: "server",
-                access: "secret",
-                optional: false,
-            }),
-            NOTION_KEY: envField.string({
-                context: "server",
-                access: "secret",
-                optional: false,
-                startsWith: "secret_",
-            }),
-        },
-    },
-    i18n: {
-        locales: ["en", "fr"],
-        defaultLocale: "en",
-    },
-    integrations: [expressiveCode(), svelte(), mdx(), react(), lit()],
-    scopedStyleStrategy: "class",
-    server: {
-        port: 8888,
-    },
-    site: "https://andrewhudson.dev",
-    devToolbar: {
-        enabled: false,
-    },
-    build: {
-        inlineStylesheets: "always",
-    },
-    // experimental: {
-    //     rustCompiler: true,
-    //     queuedRendering: {
-    //         enabled: true,
-    //         contentCache: true,
-    //     },
-    // },
+	trailingSlash: "always",
+	env: {
+		schema: {
+			NOTION_DB: envField.string({
+				context: "server",
+				access: "secret",
+				optional: false,
+			}),
+			NOTION_KEY: envField.string({
+				context: "server",
+				access: "secret",
+				optional: false,
+				startsWith: "secret_",
+			}),
+		},
+	},
+	i18n: {
+		locales: ["en", "fr"],
+		defaultLocale: "en",
+	},
+	integrations: [expressiveCode(), svelte(), mdx(), react(), lit()],
+	scopedStyleStrategy: "class",
+	server: {
+		port: 8888,
+	},
+	site: "https://andrewhudson.dev",
+	devToolbar: {
+		enabled: false,
+	},
+	build: {
+		inlineStylesheets: "always",
+	},
+	experimental: {
+		rustCompiler: true,
+		queuedRendering: {
+			enabled: true,
+			contentCache: true,
+		},
+	},
+	markdown: {
+		processor: satteri({
+			features: { directive: true },
+		}),
+	},
 });
