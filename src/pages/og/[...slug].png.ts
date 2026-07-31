80ffefffc82de3972d3979089425a84684457091
Andrew Hudson <ahudson@gmail.com>
2026-07-31 08:49:34 +0200

    add ogImage again

---
+++ src/pages/og/[...slug].png.ts
@@ -0,0 +1,108 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+import { createElement as h } from "react";
+import { renderOGImage } from "@utils/og";
+
+export async function getStaticPaths() {
+	const posts = await getCollection("blog");
+	return posts.map((post) => ({
+		params: { slug: post.id },
+		props: { title: post.data.title },
+	}));
+}
+
+export const GET: APIRoute = async ({ props }) => {
+	const { title } = props;
+
+	const blobDimension = 64;
+	const orange = "#ff4500";
+	const blobColor = "#ffc500";
+	const anotherBlobColor = "#ff003b";
+
+	const markup = h(
+		"div",
+		{
+			style: {
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "flex-end",
+				width: "100%",
+				height: "100%",
+				padding: "60px",
+				background: "#fff",
+			},
+		},
+		h(
+			"div",
+			{
+				style: {
+					display: "flex",
+					marginBottom: "24px",
+					gap: 12,
+					alignItems: "flex-end",
+				},
+			},
+			h("div", {
+				style: {
+					width: blobDimension * (1 / 3),
+					height: blobDimension * (1 / 3),
+					borderRadius: `${100 * (1 / 3)}%`,
+					background: anotherBlobColor,
+				},
+			}),
+			h("div", {
+				style: {
+					width: blobDimension * (2 / 3),
+					height: blobDimension * (2 / 3),
+					borderRadius: `${100 * (1 / 3)}%`,
+					background: blobColor,
+				},
+			}),
+			h("div", {
+				style: {
+					width: blobDimension * (1 / 3),
+					height: blobDimension * (1 / 3),
+					borderRadius: `${100 * (1 / 3)}%`,
+					background: anotherBlobColor,
+				},
+			}),
+		),
+		h(
+			"div",
+			{
+				style: {
+					display: "flex",
+					fontSize: "64px",
+					fontWeight: 800,
+					color: orange,
+					lineHeight: 1.1,
+					maxWidth: "1000px",
+				},
+			},
+			title,
+		),
+		h(
+			"div",
+			{
+				style: {
+					display: "flex",
+					marginTop: "32px",
+					fontSize: "24px",
+					color: "#737373",
+					gap: "1rem",
+				},
+			},
+			h("img", {
+				src: "https://andrewhudson.dev/images/me.png",
+				height: 30,
+				width: 30,
+				style: {
+					borderRadius: "100%",
+				},
+			}),
+			"Andrew JD Hudson",
+		),
+	);
+
+	return renderOGImage(markup);
+};
---
+++ src/utils/og.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import satori from "satori";
+import sharp from "sharp";
+
+const fontData = readFileSync(
+	join(process.cwd(), "public/fonts/Syne-Bold.ttf"),
+);
+
+const fonts = [
+	{
+		name: "Syne",
+		data: fontData,
+		weight: 700 as const,
+		style: "normal" as const,
+	},
+];
+
+export async function renderOGImage(
+	jsx: Parameters<typeof satori>[0],
+): Promise<Response> {
+	const svg = await satori(jsx, { width: 1200, height: 630, fonts });
+	const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+	return new Response(new Uint8Array(png), {
+		headers: { "Content-Type": "image/png" },
+	});
+}
